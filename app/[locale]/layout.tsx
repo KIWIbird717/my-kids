802d6cf2b008f2a3f74e818cbb012b7e9d84a6f6
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-08-06 15:41:14 +0300

    fix: prettier

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/public/styles/globals.scss"
+import "@/public/styles/globals.scss";
 import Script from "next/script";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
